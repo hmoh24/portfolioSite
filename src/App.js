e662423ba027d107280d49f606bae2c4309ec129
hmoh24 <hamsam0097@gmail.com>
2022-04-15 23:24:31 +0100

    add Home component for particles, not functioning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/header';
+import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
       <Header></Header>
+      {/* <Home></Home> */}
     </>
   )
 }
